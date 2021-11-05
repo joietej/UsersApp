1ca5508d75fae853d3b87999eb9f064ff913f249
tejas sahasrabudhe <tejas.sahasrabudhe@gmail.com>
2021-11-05 14:11:11 +0530

    Initial Commit

---
+++ FooApp/FooAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FooAppApp.swift
+//  FooApp
+//
+//  Created by Tejas Sahasrabudhe on 05/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FooAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
